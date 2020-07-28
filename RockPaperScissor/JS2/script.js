d7a11fa71a76147d2a7f307207bb0beb7ae72300
minkal-chaudhary <61204357+minkal-chaudhary@users.noreply.github.com>
2020-07-28 21:52:19 +0530

    Added Pic Generator

---
+++ RockPaperScissor/JS2/script.js
@@ -0,0 +1,10 @@
+function genCat(){
+ var image=document.createElement("img");
+ var div=document.getElementById("flex-box");
+ image.src="cc1.jpg";
+ image.width=100;
+ image.height=100;
+ div.appendChild(image);
+
+
+}
